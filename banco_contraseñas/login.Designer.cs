48a59e5af12a9dfced11d65a22b01a35a839eb4b
duban4 <duban.soporte@gmail.com>
2019-12-30 13:44:45 -0500

    se  modifica el login para encontrar el mas adecuado

--- banco_contraseñas/login.Designer.cs
+++ banco_contraseñas/login.Designer.cs
@@ -29,112 +29,115 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
-            this.btn_entrar = new System.Windows.Forms.Button();
-            this.btn_limpiar = new System.Windows.Forms.Button();
-            this.btnlogoepsi = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.txt_usuario = new System.Windows.Forms.TextBox();
             this.txt_contraseña = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_entrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // btn_entrar
+            // txt_usuario
             // 
-            this.btn_entrar.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btn_entrar.Location = new System.Drawing.Point(162, 357);
-            this.btn_entrar.Margin = new System.Windows.Forms.Padding(4);
-            this.btn_entrar.Name = "btn_entrar";
-            this.btn_entrar.Size = new System.Drawing.Size(146, 35);
-            this.btn_entrar.TabIndex = 0;
-            this.btn_entrar.Text = "ENTRAR";
-            this.btn_entrar.UseVisualStyleBackColor = true;
-            this.btn_entrar.Click += new System.EventHandler(this.Btn_entrar_Click);
+            this.txt_usuario.Location = new System.Drawing.Point(168, 205);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(155, 34);
+            this.txt_usuario.TabIndex = 13;
+            // 
+            // txt_contraseña
+            // 
+            this.txt_contraseña.Location = new System.Drawing.Point(168, 271);
+            this.txt_contraseña.Name = "txt_contraseña";
+            this.txt_contraseña.PasswordChar = '*';
+            this.txt_contraseña.Size = new System.Drawing.Size(155, 34);
+            this.txt_contraseña.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 34);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::banco_contraseñas.Properties.Resources.gg;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(120, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(42, 34);
+            this.button2.TabIndex = 20;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::banco_contraseñas.Properties.Resources.Logo_png_01;
+            this.pictureBox1.Location = new System.Drawing.Point(186, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 172);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
             // 
             // btn_limpiar
             // 
-            this.btn_limpiar.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btn_limpiar.Location = new System.Drawing.Point(162, 400);
+            this.btn_limpiar.BackgroundImage = global::banco_contraseñas.Properties.Resources.krig_Aqua_button;
+            this.btn_limpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_limpiar.FlatAppearance.BorderSize = 0;
+            this.btn_limpiar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_limpiar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_limpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_limpiar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(285, 346);
             this.btn_limpiar.Margin = new System.Windows.Forms.Padding(4);
             this.btn_limpiar.Name = "btn_limpiar";
-            this.btn_limpiar.Size = new System.Drawing.Size(146, 35);
+            this.btn_limpiar.Size = new System.Drawing.Size(155, 48);
             this.btn_limpiar.TabIndex = 1;
             this.btn_limpiar.Text = "LIMPIAR";
             this.btn_limpiar.UseVisualStyleBackColor = true;
             this.btn_limpiar.Click += new System.EventHandler(this.Btn_limpiar_Click);
             // 
-            // btnlogoepsi
+            // btn_entrar
             // 
-            this.btnlogoepsi.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnlogoepsi.BackgroundImage")));
-            this.btnlogoepsi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnlogoepsi.Enabled = false;
-            this.btnlogoepsi.Location = new System.Drawing.Point(89, 87);
-            this.btnlogoepsi.Margin = new System.Windows.Forms.Padding(2);
-            this.btnlogoepsi.Name = "btnlogoepsi";
-            this.btnlogoepsi.Size = new System.Drawing.Size(276, 159);
-            this.btnlogoepsi.TabIndex = 10;
-            this.btnlogoepsi.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 275);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 24);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "Usuario:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 310);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 24);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Contraseña:";
-            // 
-            // txt_usuario
-            // 
-            this.txt_usuario.Location = new System.Drawing.Point(153, 270);
-            this.txt_usuario.Name = "txt_usuario";
-            this.txt_usuario.Size = new System.Drawing.Size(166, 29);
-            this.txt_usuario.TabIndex = 13;
-            // 
-            // txt_contraseña
-            // 
-            this.txt_contraseña.Location = new System.Drawing.Point(153, 305);
-            this.txt_contraseña.Name = "txt_contraseña";
-            this.txt_contraseña.PasswordChar = '*';
-            this.txt_contraseña.Size = new System.Drawing.Size(166, 29);
-            this.txt_contraseña.TabIndex = 14;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.LightBlue;
-            this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(54, 13);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(348, 72);
-            this.button1.TabIndex = 17;
-            this.button1.Text = "BANCO DE CONTRASEÑAS EPSI  BIENVENID@";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btn_entrar.BackgroundImage = global::banco_contraseñas.Properties.Resources.krig_Aqua_button;
+            this.btn_entrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_entrar.FlatAppearance.BorderSize = 0;
+            this.btn_entrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_entrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_entrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_entrar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_entrar.Location = new System.Drawing.Point(13, 346);
+            this.btn_entrar.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_entrar.Name = "btn_entrar";
+            this.btn_entrar.Size = new System.Drawing.Size(155, 48);
+            this.btn_entrar.TabIndex = 0;
+            this.btn_entrar.Text = "ENTRAR";
+            this.btn_entrar.UseVisualStyleBackColor = true;
+            this.btn_entrar.Click += new System.EventHandler(this.Btn_entrar_Click);
             // 
             // login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Azure;
-            this.ClientSize = new System.Drawing.Size(457, 483);
+            this.ClientSize = new System.Drawing.Size(453, 407);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txt_contraseña);
             this.Controls.Add(this.txt_usuario);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnlogoepsi);
             this.Controls.Add(this.btn_limpiar);
             this.Controls.Add(this.btn_entrar);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(6);
@@ -142,6 +145,7 @@
             this.Name = "login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = " Expertos Profesionales en Servicios Sociales Integrales";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,12 +155,11 @@
 
         private System.Windows.Forms.Button btn_entrar;
         private System.Windows.Forms.Button btn_limpiar;
-        private System.Windows.Forms.Button btnlogoepsi;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txt_usuario;
         private System.Windows.Forms.TextBox txt_contraseña;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
