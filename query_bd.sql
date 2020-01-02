e7fcb1ade0a9ae104c2708f8e565a08949a460c7
carlos-soporte <59293341+carlos-soporte@users.noreply.github.com>
2020-01-02 17:26:15 -0500

    Update query_bd.sql
    se crean los procedimientos almacenados para actualizar y eliminar registros

--- query_bd.sql
+++ query_bd.sql
@@ -1,9 +1,11 @@
+--lineas para borrar la base de datos.
 USE MASTER
 DROP DATABASE banco_contraseña
+
+----------------------------------------------codigo para ejecutar-------------------------------------------
 --se crea la base de datos
 CREATE DATABASE banco_contraseña;
 GO
-
 
 --se pasa de la bd master a la bd creada
 USE banco_contraseña;
@@ -12,7 +14,7 @@ GO
 --se crea la tabla para usuarios habilitados
 CREATE TABLE habilitados
 (
-usuario VARCHAR(20) NOT NULL,
+usuario VARCHAR(25) NOT NULL,
 contraseña VARCHAR(15) NOT NULL
 )
 GO
@@ -26,38 +28,94 @@ GO
 CREATE TABLE banco
 (
 id_registro INT IDENTITY NOT NULL,
-entidad VARCHAR(25) NOT NULL,
-descripcion VARCHAR(50) NOT NULL,
-usuario VARCHAR(30) NOT NULL,
+entidad VARCHAR(20) NOT NULL,
+descripcion VARCHAR(40) NOT NULL,
+usuario VARCHAR(25) NOT NULL,
 contraseña VARCHAR(20) NOT NULL
 )
 GO
 
+--se asigna la clave primaria para 
 ALTER TABLE banco
 ADD PRIMARY KEY(id_registro)
 GO
 
 --se crea un procedimiento almacenado para insertar usuarios habilitados
 CREATE PROCEDURE insertar_habilitado
-@usuario VARCHAR(20),
-@contraseña VARCHAR(15)
+@usuario VARCHAR(25),
+@contraseña VARCHAR(20)
 AS
 INSERT INTO habilitados(usuario,contraseña) VALUES (@usuario,@contraseña)
-GO
-
---se hacen dos registros de usuarios habilitados despues de ejecutado el procedimiento
-EXEC insertar_habilitado 'carlos','1234'
-EXEC insertar_habilitado 'duban','4321'
 GO
 
 --se hace un procedimiento almacenado para añadir una entidad completa al banco de contraseñas
 CREATE PROCEDURE insertar_banco
 @entidad VARCHAR(20),
-@descripcion VARCHAR(50),
+@descripcion VARCHAR(40),
 @usuario VARCHAR(25),
-@contraseña VARCHAR(15)
+@contraseña VARCHAR(20)
 AS
 INSERT INTO banco(entidad,descripcion,usuario,contraseña) VALUES (@entidad,@descripcion,@usuario,@contraseña)
+GO
+
+--procedimiento para validar el usuario habilitado para ingresar al sistema
+CREATE PROCEDURE consultar_usuario
+@usuario VARCHAR(25),
+@contraseña VARCHAR(20)
+AS
+SELECT usuario,contraseña from habilitados where usuario=@usuario and contraseña=@contraseña
+GO
+
+--procedimiento para listar los campos a trabjajar
+CREATE PROCEDURE listar_banco
+AS
+SELECT id_registro,entidad,descripcion,usuario,contraseña FROM banco
+GO
+--procedimiento almacenado para filtrar por entidad
+CREATE PROCEDURE filtrar_banco
+@entidad VARCHAR(20)
+AS
+SELECT * FROM banco WHERE entidad LIKE  '%'+ @entidad +'%'
+GO
+
+--procedimiento para actualizar contraseña del banco de contraseñas.
+CREATE PROCEDURE editar_contraseña
+@id_registro INT,
+@contraseña VARCHAR(20)
+AS
+UPDATE banco SET contraseña=@contraseña WHERE id_registro=@id_registro
+GO
+
+--procedimiento para actualizar usuario del banco de contraseñas.
+CREATE PROCEDURE editar_usuario
+@id_registro INT,
+@usuario VARCHAR(25)
+AS
+UPDATE banco SET usuario=@usuario WHERE id_registro=@id_registro
+GO
+
+--procedimiento para actualizar usuario y contraseña del banco de contraseñas.
+CREATE PROCEDURE editar_duo
+@id_registro INT,
+@usuario VARCHAR(25),
+@contraseña VARCHAR(20)
+AS
+UPDATE banco SET usuario=@usuario,contraseña=@contraseña WHERE id_registro=@id_registro
+GO
+
+--procedimiento para borrar un registro del banco de contraseñas.
+CREATE PROCEDURE eliminar_banco
+@id_registro INT
+AS
+DELETE FROM banco WHERE id_registro=@id_registro
+GO
+
+
+
+-----------------------------------REGISTROS-----------------------------------------------
+--se hacen dos registros de usuarios habilitados despues de ejecutado el procedimiento
+EXEC insertar_habilitado 'carlos','1234'
+EXEC insertar_habilitado 'duban','4321'
 GO
 
 --se hacen 15 registros con fines practicos.
@@ -76,23 +134,3 @@ EXEC insertar_banco 'culito nuevo','te la creiste, solo seriedad','pepe_grillo',
 EXEC insertar_banco 'vecina amable','digale que apague el equipo','pepe_grillo','chao32145' 
 EXEC insertar_banco 'inventenflix','habia que colocar algo','pepe_grillo','chao32145' 
 EXEC insertar_banco 'tunel del gato','si no sabe de tuneles, no opine','pepe_grillo','chao32145' 
-GO
---procedimiento para validar el usuario habilitado para ingresar al sistema
-CREATE PROCEDURE consultar_usuario
-@usuario VARCHAR(20),
-@contraseña VARCHAR(15)
-AS
-SELECT usuario,contraseña from habilitados where usuario=@usuario and contraseña=@contraseña
-GO
-
-
---procedimiento para listar los campos a trabjajar
-CREATE PROCEDURE listar_banco
-AS
-SELECT * FROM banco
-GO
---procedimiento almacenado para filtrar por entidad
-CREATE PROCEDURE filtrar_banco
-@entidad VARCHAR(25)
-AS
-SELECT * FROM banco WHERE entidad LIKE  '%'+ @entidad +'%'
