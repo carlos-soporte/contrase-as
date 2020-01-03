ec822b74c7bbf16b5069920d601f4e7a36ab1e63
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-01-03 11:39:45 -0500

    Revert "Revert "programación frmEliminar""
    This reverts commit 303e206b74a1ed7fe329e55b90aa60cbda1ac47b.

--- banco_contraseñas/FrmConsultar.cs
+++ banco_contraseñas/FrmConsultar.cs
@@ -12,6 +12,7 @@ namespace banco_contraseñas
 {
     public partial class FrmConsultar : Form
     {
+        public int id_registro;
         public FrmConsultar()
         {
             InitializeComponent();
@@ -32,6 +33,7 @@ namespace banco_contraseñas
 
         public DataSet llenarGv()
         {
+            
             DataSet ds;
 
             string query = "EXEC listar_banco";
@@ -84,6 +86,20 @@ namespace banco_contraseñas
             }
             MessageBox.Show("usted ha salido de la aplicacion");
             Application.Exit();
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            try
+            {
+                id_registro = (int)dataGridView1.Rows[e.RowIndex].Cells[0].Value;
+            }
+            catch (Exception)
+            {
+
+            }
+            
         }
     }
 }
