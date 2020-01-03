b4b2839aaa338adeb24d2d08a52fc44a10d6814b
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-01-03 11:00:53 -0500

    cambio de longitud texbox
    se redimensiona los texbox de frm agregar

--- banco_contraseñas/FrmAgregar.Designer.cs
+++ banco_contraseñas/FrmAgregar.Designer.cs
@@ -112,6 +112,7 @@
             // 
             this.txtUsuario.Location = new System.Drawing.Point(211, 190);
             this.txtUsuario.Margin = new System.Windows.Forms.Padding(6);
+            this.txtUsuario.MaxLength = 25;
             this.txtUsuario.Name = "txtUsuario";
             this.txtUsuario.Size = new System.Drawing.Size(327, 29);
             this.txtUsuario.TabIndex = 22;
@@ -120,6 +121,7 @@
             // 
             this.txtClave.Location = new System.Drawing.Point(211, 254);
             this.txtClave.Margin = new System.Windows.Forms.Padding(6);
+            this.txtClave.MaxLength = 20;
             this.txtClave.Name = "txtClave";
             this.txtClave.Size = new System.Drawing.Size(327, 29);
             this.txtClave.TabIndex = 23;
@@ -128,6 +130,7 @@
             // 
             this.txtDescripcion.Location = new System.Drawing.Point(211, 128);
             this.txtDescripcion.Margin = new System.Windows.Forms.Padding(6);
+            this.txtDescripcion.MaxLength = 40;
             this.txtDescripcion.Name = "txtDescripcion";
             this.txtDescripcion.Size = new System.Drawing.Size(327, 29);
             this.txtDescripcion.TabIndex = 24;
@@ -136,6 +139,7 @@
             // 
             this.txtEntidad.Location = new System.Drawing.Point(211, 64);
             this.txtEntidad.Margin = new System.Windows.Forms.Padding(6);
+            this.txtEntidad.MaxLength = 20;
             this.txtEntidad.Name = "txtEntidad";
             this.txtEntidad.Size = new System.Drawing.Size(327, 29);
             this.txtEntidad.TabIndex = 25;
