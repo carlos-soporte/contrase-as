de7672cfb680615c02c87e4bcec74d78060e0f28
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-01-03 12:52:22 -0500

    se intenta organizar còdigo

--- banco_contraseñas/Program.cs
+++ banco_contraseñas/Program.cs
@@ -16,7 +16,7 @@ namespace banco_contraseñas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmActualizar());
+            Application.Run(new login());
         }
     }
 }
