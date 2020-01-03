512715c5b60e00031bc47acc3f90149631fe2602
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-01-03 16:29:09 -0500

    creacion y preparaciòn frmEliminar

---
+++ banco_contraseñas/FrmEliminar.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace banco_contraseñas
+{
+    partial class FrmEliminar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmEliminar));
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btn_actualizar = new System.Windows.Forms.Button();
+            this.btn_buscar = new System.Windows.Forms.Button();
+            this.txt_filtrar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_menu = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnlogoepsi = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Gold;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(289, 1);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(119, 32);
+            this.btnSalir.TabIndex = 46;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btn_actualizar
+            // 
+            this.btn_actualizar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_actualizar.Location = new System.Drawing.Point(558, 207);
+            this.btn_actualizar.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_actualizar.Name = "btn_actualizar";
+            this.btn_actualizar.Size = new System.Drawing.Size(146, 33);
+            this.btn_actualizar.TabIndex = 45;
+            this.btn_actualizar.Text = "REFRESCAR";
+            this.btn_actualizar.UseVisualStyleBackColor = true;
+            this.btn_actualizar.Click += new System.EventHandler(this.btn_actualizar_Click);
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_buscar.Location = new System.Drawing.Point(395, 205);
+            this.btn_buscar.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(146, 33);
+            this.btn_buscar.TabIndex = 44;
+            this.btn_buscar.Text = "BUSCAR";
+            this.btn_buscar.UseVisualStyleBackColor = true;
+            this.btn_buscar.Click += new System.EventHandler(this.btn_buscar_Click);
+            // 
+            // txt_filtrar
+            // 
+            this.txt_filtrar.Location = new System.Drawing.Point(128, 207);
+            this.txt_filtrar.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txt_filtrar.Name = "txt_filtrar";
+            this.txt_filtrar.Size = new System.Drawing.Size(245, 29);
+            this.txt_filtrar.TabIndex = 43;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 207);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 22);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Entidad:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Azure;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Azure;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 261);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1000, 330);
+            this.dataGridView1.TabIndex = 47;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btn_menu
+            // 
+            this.btn_menu.BackColor = System.Drawing.Color.Gold;
+            this.btn_menu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_menu.Location = new System.Drawing.Point(1, 1);
+            this.btn_menu.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_menu.Name = "btn_menu";
+            this.btn_menu.Size = new System.Drawing.Size(280, 32);
+            this.btn_menu.TabIndex = 41;
+            this.btn_menu.Text = "Volver al menú principal";
+            this.btn_menu.UseVisualStyleBackColor = false;
+            this.btn_menu.Click += new System.EventHandler(this.btn_menu_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightBlue;
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(135, 140);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 48);
+            this.button2.TabIndex = 40;
+            this.button2.Text = "CONSULTA";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightBlue;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(320, 48);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(348, 66);
+            this.button1.TabIndex = 39;
+            this.button1.Text = "BANCO DE CONTRASEÑAS EPSI ";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnlogoepsi
+            // 
+            this.btnlogoepsi.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnlogoepsi.BackgroundImage")));
+            this.btnlogoepsi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnlogoepsi.Enabled = false;
+            this.btnlogoepsi.Location = new System.Drawing.Point(827, 11);
+            this.btnlogoepsi.Margin = new System.Windows.Forms.Padding(2);
+            this.btnlogoepsi.Name = "btnlogoepsi";
+            this.btnlogoepsi.Size = new System.Drawing.Size(196, 113);
+            this.btnlogoepsi.TabIndex = 38;
+            this.btnlogoepsi.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnEliminar.Location = new System.Drawing.Point(712, 207);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(304, 33);
+            this.btnEliminar.TabIndex = 48;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // FrmEliminar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(1034, 621);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btn_actualizar);
+            this.Controls.Add(this.btn_buscar);
+            this.Controls.Add(this.txt_filtrar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_menu);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnlogoepsi);
+            this.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.MaximizeBox = false;
+            this.Name = "FrmEliminar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmEliminar";
+            this.Load += new System.EventHandler(this.FrmEliminar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btn_actualizar;
+        private System.Windows.Forms.Button btn_buscar;
+        private System.Windows.Forms.TextBox txt_filtrar;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_menu;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnlogoepsi;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
---
+++ banco_contraseñas/FrmEliminar.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace banco_contraseñas
+{
+    public partial class FrmEliminar : Form
+    {
+        public int id_registro;
+        public FrmEliminar()
+        {
+            InitializeComponent();
+        }
+
+
+        public DataSet llenarGv()
+        {
+
+            DataSet ds;
+
+            string query = "EXEC listar_banco";
+            ds = bd.consultar(query);
+
+            return ds;
+        }
+
+        private void btn_menu_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new lista_opciones().Show();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult respuesta = MessageBox.Show("¿seguro que desea salir?", "SALIR", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+
+            if (respuesta == DialogResult.Yes)
+            {
+                MessageBox.Show("usted ha salido de la aplicacion");
+                Application.Exit();
+            }
+            else if (respuesta == DialogResult.No)
+            {
+            }
+            else if (respuesta == DialogResult.Cancel)
+            {
+            }
+            MessageBox.Show("usted ha salido de la aplicacion");
+            Application.Exit();
+        }
+
+        private void btn_actualizar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = llenarGv().Tables[0];
+            txt_filtrar.Text = "";
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_filtrar.Text.Trim()) == false)
+            {
+                try
+                {
+                    DataSet ds;
+
+                    string query = "EXEC filtrar_banco '" + txt_filtrar.Text + "'";
+                    ds = bd.consultar(query);
+                    dataGridView1.DataSource = ds.Tables[0];
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Ha ocurrido un error " + ex.Message);
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                id_registro = (int)dataGridView1.Rows[e.RowIndex].Cells[0].Value;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void FrmEliminar_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = llenarGv().Tables[0];
+        }
+    }
+}
