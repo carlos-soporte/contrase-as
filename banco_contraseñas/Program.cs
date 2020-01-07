818799c30f6927a6d2244f82acf769914a356112
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-01-07 17:05:06 -0500

    CONFIRMACIÒN PROYECTO

--- banco_contraseñas/Program.cs
+++ banco_contraseñas/Program.cs
@@ -16,7 +16,7 @@ namespace banco_contraseñas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new lista_opciones());
+            Application.Run(new login());
         }
     }
 }
